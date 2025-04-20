c5b8c1cdafc11f5fc584f91910042405689c44d2
HyeYeon <skojphy@gmail.com>
2025-04-20 20:16:15 +0900

    ♿️ Cell.svelte 접근성(a11y) 개선
    - <div>+<input> 구조를 button[role="textbox"]로 변경
    - aria-label, tabindex 등 추가로 Svelte a11y 경고 제거
    - 스타일 및 사용성 유지, slot으로 내용 삽입 가능

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -1,18 +1,26 @@
-<div>
-	<input type="text" placeholder="Insert Nmae" />
-</div>
+<button
+	type="button"
+	class="cell-input"
+	placeholder="Insert Name"
+	role="textbox"
+	tabindex="0"
+	aria-label="셀 이름 입력"
+	style="width:100%;background:none;border:none;outline:none;"
+>
+	<slot />
+</button>
 
 <style>
-	div {
+	button.cell-input {
 		display: flex;
 		align-items: center;
 		background-color: rgb(255, 201, 201);
 		width: 80px;
 		height: 80px;
 		border: 1px gray solid;
-	}
-	input {
-		width: 75px;
 		padding: 0;
+		font: inherit;
+		border-radius: 0;
+		cursor: pointer;
 	}
 </style>
